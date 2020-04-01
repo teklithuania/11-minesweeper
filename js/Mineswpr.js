fadda6002acc8166c4a37035d48613d71472f0b9
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-04-01 11:27:46 +0300

    Game logic #2

--- js/Mineswpr.js
+++ js/Mineswpr.js
@@ -1,19 +1,20 @@
-class Minesweeeper {
-    constructor(target, width, height, bombsPercentage) {
+class Minesweeper {
+    constructor( target, width, height, bombsPercentage ) {
         this.target = target;
         this.DOM = null;
         this.width = width;
         this.height = height;
         this.bombsPercentage = bombsPercentage;
-        this.bombsPercentageCount = 1;
-        
+        this.bombsCount = 1;
+
         this.init();
     }
 
-    init(){
+    init() {
         this.validate();
         this.render();
     }
+
     validate() {
         // check if valid target selector
         const DOM = document.querySelector(this.target);
@@ -23,18 +24,22 @@ class Minesweeeper {
         this.DOM = DOM;
 
         // check for correct size
-        if ( typeof(this.width) !== 'number' || this.width < 1 ||
-        this.width % 1 > 0 ) {
+        if ( typeof(this.width) !== 'number' ||
+             this.width < 1 ||
+             this.width % 1 > 0 ) {
             throw 'Netinkamas plotis!';
         }
-        if ( typeof(this.height) !== 'number' || this.height < 1 ||
-        this.height % 1 > 0 ) {
+        if ( typeof(this.height) !== 'number' ||
+             this.height < 1 ||
+             this.height % 1 > 0 ) {
             throw 'Netinkamas aukstis!';
         }
         if ( this.width * this.height < 2 ) {
-            throw 'Bendras lentos plotis yra per mazas.';
+            throw 'Bendras lentos plotas yra per mazas.';
         }
-        if ( typeof(this.bombsPercentage) !== 'number' || this.bombsPercentage <= 0 || this.bombsPercentage >= 100 ) {
+        if ( typeof(this.bombsPercentage) !== 'number' ||
+             this.bombsPercentage <= 0 ||
+             this.bombsPercentage >= 100 ) {
             throw 'Netinkamas bombu kiekis!';
         }
 
@@ -44,6 +49,7 @@ class Minesweeeper {
             this.bombsCount = bombsCount;
         }
     }
+
     render() {
         let HTML = `<div class="header">
                         <div class="bombs">099</div>
@@ -51,19 +57,19 @@ class Minesweeeper {
                         <div class="timer">000</div>
                     </div>
                     <div class="field">
-                        <div class="cell">c</div>
-                        <div class="cell">c</div>
-                        <div class="cell">c</div>
-                        <div class="cell">c</div>
-                        <div class="cell">c</div>
-                        <div class="cell">c</div>
+                        <div class="cell">C</div>
+                        <div class="cell">C</div>
+                        <div class="cell">C</div>
+                        <div class="cell">C</div>
+                        <div class="cell">C</div>
+                        <div class="cell">C</div>
+                        <div class="cell">C</div>
                     </div>`;
-        this.DOM.classList.add('minesweeper')
+        this.DOM.classList.add('minesweeper');
         this.DOM.innerHTML = HTML;
     }
 }
 
-
-const game = new Minesweeeper('#game', 10, 10, 47);
+const game = new Minesweeper('#game', 10, 10, 15);
 
 console.log(game);
